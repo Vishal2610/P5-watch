310669c71f08898e86a89029943da1282a9eda01
Vishal2610 <vishal.tdm@gmail.com>
2017-10-10 07:05:17 +0530

    precise minutes and hours

--- watch/sketch.js
+++ watch/sketch.js
@@ -4,51 +4,56 @@ function setup() {
 }
 
 function draw(){
-    background(150, 150, 150);
+    background(0, 0, 0);
     translate(300, 300);
     rotate(-90);
 
-    let hr = hour();
-    let min = minute();
+    let hrs = hour();
+    let mins = minute();
     let sec = second();
+	
+	
+	
+	let min = mins + "." + sec;
+	let hr = hrs + "." + mins;
 
     strokeWeight(8);
-    stroke(255,125,255);
+    stroke(255,75,255);
     noFill();
     let secHand = map(sec, 0, 60, 0, 360);
     arc(0, 0, 300, 300, 0, secHand);
     
     push();
     rotate(secHand);
-    stroke(255,125,255);
+    stroke(255,75,255);
     line(0, 0, 100, 0);
     pop();
 
     strokeWeight(8);
-    stroke(125, 255, 255);
+    stroke(75, 255, 255);
     noFill();
     let minHand = map(min, 0, 60, 0, 360);
     arc(0, 0, 275, 275, 0, minHand);
 
     push();
     rotate(minHand);
-    stroke(125, 255, 255);
+    stroke(75, 255, 255);
     line(0, 0, 80, 0);
     pop();
 
     strokeWeight(8);
-    stroke(255, 255, 125);
+    stroke(255, 255, 75);
     noFill();
-    let hrHand = map(hr, 0, 60, 0, 360);
+    let hrHand = map(hr%12, 0, 12, 0, 360);
     arc(0, 0, 250, 250, 0, hrHand);
 
     push();
     rotate(hrHand);
-    stroke(255, 255, 125);
+    stroke(255, 255, 75);
     line(0, 0, 60, 0);
     pop();
 
-    stroke(150);
+    stroke(0);
     line(0, 0, 0, 0);
 
 }
